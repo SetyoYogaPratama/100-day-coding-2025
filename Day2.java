19091bc6968567290418aab2dc06215db84677ed
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-10 02:10:49 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class day2 {
+    
+    public static void main(String[] args) {
+        //println menampilkan teks lalu otomatis ganti baris.
+        System.out.println("Halo, bang");
+        System.out.println("Apa kabar?");
+        
+        //print menampilkan teks tanpa ganti baris
+        System.out.print("Halo, bang");
+        System.out.print("Apa kabar?");
+        
+        //printf menampilkan teks dengan format tertentu
+        int umur = 20;
+        double ipk = 3.75;
+        String nama = "SETYO";
+        System.out.printf("Nama: %s%nUmur: %d%nIPK: %.2f%n", nama, umur, ipk);
+    }
+    
+          }
