6214904da9585117330ec1c328f487fb62e8249e
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-10 21:10:55 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,11 @@
+public class day3 {
+    
+    public static void main(String[] args) {
+        //komentar tanpa baris
+        /*komentar 
+         * lebih dari
+         * 1baris
+         */
+    }
+    
+}
