28d6f8ecebdb38f1f652fad1b3e475ffe752d148
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-11 20:29:20 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -1,22 +1,26 @@
 public class day_4 {
     
     public static void main(String[] args) {
-        // Menampilkan identitas menggunakan escape sequence
+        // \n -> pindah ke baris baru
+        System.out.println("Halo!\nPerkenalkan identitas saya:");
 
-        System.out.println("=== Identitas Diri ===");
-
-        // \n digunakan untuk pindah ke baris baru
-        System.out.println("Nama    : Setyo Yoga Pratama\nProdi   : Informatika\nKampus  : UNSULBAR");
-
-        // \t digunakan untuk memberi jarak/tab agar rapi
-        System.out.println("\n--- Format dengan Tab ---");
+        // \t -> bikin jarak/tab
         System.out.println("Nama\t: Setyo Yoga Pratama");
         System.out.println("Prodi\t: Informatika");
         System.out.println("Kampus\t: UNSULBAR");
 
-        // Gabungan \n dan \t untuk format tabel sederhana
-        System.out.println("\n--- Format Tabel ---");
-        System.out.println("\tNama\t: Setyo Yoga Pratama\n\tProdi\t: Informatika\n\tKampus\t: UNSULBAR");
+        // \" dan \' -> nampilin tanda kutip
+        System.out.println("Kata dosen: \"Belajar itu jangan setengah hati.\"");
+        System.out.println("Tulisan: \'Mahasiswa Informatika UNSULBAR\'");
+
+        // \\ -> nampilin backslash
+        System.out.println("Folder tugas: C:\\Users\\Setyo\\Documents");
+
+        // \r -> balik ke awal baris, teks sebelumnya bisa ketimpa
+        System.out.println("Belajar Java\rMantap!"); // output: "Mantap! Java"
+
+        // \b -> hapus satu karakter sebelumnya
+        System.out.println("Helloo\b World"); // output: "Hello World"
     }
     
 }
