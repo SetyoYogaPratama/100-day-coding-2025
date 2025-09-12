3f9fe3fa6e8037867cd8f9f5c8e3a34041561a67
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-12 23:08:10 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class day_5 {
+    
+    public static void main(String[] args) {
+        byte umur = 20;
+        short tahunLahir = 2005;
+        int jumlahMahasiswa = 15000;
+        long populasiDunia = 7800000000L;
+        
+        System.out.println("umur  : " + umur);
+        System.out.println("tahun lahir   : " + tahunLahir);
+        System.out.println("jumlah mahasiswa : " + jumlahMahasiswa);
+        System.out.println("populasi dunia : " + populasiDunia);
+    }
+    
+}
