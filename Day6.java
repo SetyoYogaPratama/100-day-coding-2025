f8d99feba3ac56ededda95e54936e773d03de3a7
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-14 19:30:14 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+public class day6 {
+    
+    public static void main(String[] args) {
+        float angkaFloat = 1.123456789f;
+        double angkaDouble = 1.1234567890123456789;
+        
+        System.out.println("Nilai float  : " + angkaFloat);
+        System.out.println("Nilai double : " + angkaDouble);
+    }
+    
+}
