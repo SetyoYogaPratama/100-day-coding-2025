c76bfb2bc6bd4fde811f77defb7948b597fea0a7
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-14 19:34:14 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,12 @@
+public class day7 {
+    
+    public static void main(String[] args) {
+        char huruf = 'A';
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Karakter: " + huruf);
+        System.out.println("Boolean benar: " + benar);
+        System.out.println("Boolean salah: " + salah);
+    }
+}
