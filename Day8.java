4e6bc17c49f880c07922bd489560f6a98cc449ca
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-15 23:31:40 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,10 @@
+public class day_8 {
+    
+    public static void main(String[] args) {
+        String depan = "Setyo Yoga Pratama";
+        String belakang = "D0224307";
+        String hasil = depan + " " + belakang;  
+        System.out.println(hasil);  
+    }
+    
+}
