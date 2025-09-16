70011b1a0d32199fe1ced9c3f58924ca35bb914c
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-16 23:24:05 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,27 @@
+public class day9 {
+    
+    public static void main(String[] args) {
+        // Deklarasi
+        int umur;
+        String nama;
+
+        // Inisialisasi
+        umur = 18;
+        nama = "Setyo Yoga Pratama";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur awal: " + umur);
+
+        // Update
+        umur = 19;
+        System.out.println("Umur setelah update: " + umur);
+
+        // Deklarasi + Inisialisasi langsung
+        double tinggi = 170.5;
+        boolean isMahasiswa = true;
+
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Status mahasiswa: " + isMahasiswa);
+    }
+    
+}
