8ecfa341c20842a44640ce338e29ed9297f6ee5d
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-22 20:29:39 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class day15 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        int hasilTambah = a + b; // penjumlahan
+        int hasilKurang = a - b; // pengurangan
+
+        System.out.println("Hasil " + a + " + " + b + " = " + hasilTambah);
+        System.out.println("Hasil " + a + " - " + b + " = " + hasilKurang);
+    
+    }
+    
+}
