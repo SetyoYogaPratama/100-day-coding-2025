e5fc85aa8b81a8c56e05738525648e017c670575
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-22 20:32:28 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,11 @@
+public class day14 {
+    
+    public static void main(String[] args) {
+        String strAngka = "123";
+
+        int angka = Integer.parseInt(strAngka); // ubah String -> int
+
+        System.out.println("Hasil konversi: " + angka);
+    }
+    
+}
