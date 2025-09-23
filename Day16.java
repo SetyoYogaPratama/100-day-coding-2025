67e65a985ba66b5ada9b492e044ed74b25e45d85
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-23 22:25:48 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,15 @@
+public class day16 {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        int hasilTambah = a * b; // perkalian
+        int hasilKurang = a % b; // pembagian
+
+        System.out.println("Hasil " + a + " X " + b + " = " + hasilTambah);
+        System.out.println("Hasil " + a + " ÷ " + b + " = " + hasilKurang);
+        
+    }
+    
+}
