62652c06a1d251ec264b67fb85fa99f45dd886fc
SetyoYogaPratama <githubakun707@gmail.com>
2025-09-24 06:58:32 -0700

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,10 @@
+public class day17 {
+  public static void main (String [] args){
+    int a = 20;
+    int b = 3;
+
+    int sisabagi = a % b;
+    System.out.println("hasil sisa bagi: " + sisabagi);
+  }
+
+}
